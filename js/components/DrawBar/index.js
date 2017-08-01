1e51d9d53b440abec677d8f6d41d2cb0746f3548
Necmettin <necmettincimen01@outlook.com>
2017-08-01 15:06:21 +0300

    network menus

--- js/components/DrawBar/index.js
+++ js/components/DrawBar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppRegistry, Image, TouchableOpacity } from "react-native";
+import { ListView, ActivityIndicator, View, AppRegistry, Image, TouchableOpacity } from "react-native";
 import {
   Button,
   Text,
@@ -11,31 +11,62 @@ import {
 } from "native-base";
 const routes = [
   {
-    title:"Belediye Başkanımız",
-    screen:"Home",
-    icon:"person"
+    title: "Belediye Başkanımız",
+    screen: "Home",
+    icon: "person"
   },
-    {
-    title:"Kurumsal",
-    screen:"BlankPage2",
-    icon:"home"
-    }
-  ];
+  {
+    title: "Kurumsal",
+    screen: "BlankPage2",
+    icon: "home"
+  }
+];
 export default class DrawBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    return fetch('http://necmettincimen-001-site1.itempurl.com/api/tCategory/1')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.setState({
+          isLoading: false,
+          dataSource: ds.cloneWithRows(responseJson),
+        }, function () {
+          // do something with new state
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   static navigationOptions = {
     header: null
   };
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, paddingTop: 20 }}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
     return (
       <Container>
         <Content>
           <Image
             source={{
-              uri: "https://www.google.com.tr/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+              uri: "http://tedarik.malatya.bel.tr/Assets/Images/webtvlogo.png"
             }}
             style={{
-              height: 120,
-              alignSelf: "stretch",
+              height: 80,
+              resizeMode:"contain",
               justifyContent: "center",
               alignItems: "center"
             }}
@@ -49,22 +80,21 @@ export default class DrawBar extends React.Component {
               }}
               onPress={() => this.props.navigation.navigate("DrawerClose")}
             >
-              
+
             </TouchableOpacity>
           </Image>
-          <List
-            dataArray={routes}
-            renderRow={data => {
-              return (
-                <ListItem
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={(rowData) =><ListItem
                   button
-                  onPress={() => this.props.navigation.navigate(data.screen)}
+                  onPress={() => this.props.navigation.navigate("BlankPage2")}
                 >
-                  <Icon name={data.icon} /><Text>  {data.title}</Text>
-                </ListItem>
-              );
-            }}
+                  <Text>  {rowData}</Text>
+                </ListItem>}
           />
+
+      
+
         </Content>
       </Container>
     );
